1680a0add765a596590578a30a3b18a52637e9d5
ramadan8 <79653940+ramadan8@users.noreply.github.com>
2021-07-23 23:07:36 +0200

    Add menu dragging.

--- Ohm/GUI/Menu.cpp
+++ Ohm/GUI/Menu.cpp
@@ -4,26 +4,64 @@
 #include "../Interfaces.h"
 #include "../InterfaceDep.h"
 
+#define MENU_WIDTH 300
+#define MENU_HEIGHT 200
+
+#define TITLEBAR_WIDTH MENU_WIDTH
+#define TITLEBAR_HEIGHT 20
+
+bool IsInRegion(int x, int y, int tx, int ty, int tw, int th) {
+	return (tx <= x && x <= tx + tw) && (ty <= y && y <= ty + th);
+}
+
 Menu::Menu() {}
 
 void Menu::Render() {
-	if (!this->is_open)
+	if (!is_open)
 		return;
+
+	// Check if we're dragging.
+	interfaces->InputSystem->GetCursorPosition(&mouse_x, &mouse_y);
+
+	if (is_dragging) {
+		if (is_clicking) {
+			offset_x = (mouse_x - previous_mouse_x) + old_offset_x;
+			offset_y = (mouse_y - previous_mouse_y) + old_offset_y;
+		} else {
+			previous_mouse_x = mouse_x;
+			previous_mouse_y = mouse_y;
+
+			old_offset_x = offset_x;
+			old_offset_y = offset_y;
+
+			is_dragging = false;
+		}
+	} else {
+		is_dragging = IsInRegion(mouse_x, mouse_y, offset_x, offset_y, TITLEBAR_WIDTH, TITLEBAR_HEIGHT);
+	}
 
 	// Background [full]
 	interfaces->Surface->DrawSetColor(Color(32, 32, 32, 255));
-	interfaces->Surface->DrawFilledRect(this->x, this->y, this->x + 300, this->y + 200);
+	interfaces->Surface->DrawFilledRect(offset_x, offset_y, offset_x + MENU_WIDTH, offset_y + MENU_HEIGHT);
 
 	// Background [outline]
 	interfaces->Surface->DrawSetColor(Color(0, 0, 0, 255));
-	interfaces->Surface->DrawOutlinedRect(this->x, this->y, this->x + 300, this->y + 200);
+	interfaces->Surface->DrawOutlinedRect(offset_x, offset_y, offset_x + MENU_WIDTH, offset_y + MENU_HEIGHT);
 
 	// Titlebar [outline]
 	interfaces->Surface->DrawSetColor(Color(0, 0, 0, 255));
-	interfaces->Surface->DrawOutlinedRect(this->x, this->y, this->x + 300, this->y + 20);
+	interfaces->Surface->DrawOutlinedRect(offset_x, offset_y, offset_x + TITLEBAR_WIDTH, offset_y + TITLEBAR_HEIGHT);
 
 	// Titlebar [text]
-	render->Text(L"Ohm v0.1.0", this->x + 6, this->y + 6, render->font_base, Color(255, 255, 255, 255));
+	render->Text(L"Ohm v0.1.0", offset_x + 6, offset_y + 6, render->font_base, Color(255, 255, 255, 255));
+
+	// Titlebar [close button]
+	interfaces->Surface->DrawSetColor(Color(255, 0, 0, 255));
+	interfaces->Surface->DrawFilledRect(offset_x + TITLEBAR_WIDTH - 20, offset_y, offset_x + TITLEBAR_WIDTH, offset_y + 20);
+
+	// Titlebar [close button outline]
+	interfaces->Surface->DrawSetColor(Color(0, 0, 0, 255));
+	interfaces->Surface->DrawOutlinedRect(offset_x + TITLEBAR_WIDTH - 20, offset_y, offset_x + TITLEBAR_WIDTH, offset_y + 20);
 }
 
 void Menu::Controls(UINT msg, WPARAM wParam, LPARAM lParam) {
--- Ohm/GUI/Menu.h
+++ Ohm/GUI/Menu.h
@@ -11,8 +11,12 @@ public:
 
 	bool is_open = true;
 	bool is_clicking = false;
+	bool is_dragging = false;
 private:
-	int x = 50, y = 50;
+	int offset_x = 50, offset_y = 50;
+	int mouse_x, mouse_y;
+	int previous_mouse_x, previous_mouse_y;
+	int old_offset_x, old_offset_y;
 };
 
 inline std::unique_ptr<Menu> menu;
